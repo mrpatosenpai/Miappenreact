44fbbe9cff67312db33ae4304108318f3bd04556
jdavidfa0 <jdavidfa0@gmail.com>
2024-06-28 10:57:46 -0500

    segundo intento

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
 const db={
-    host: "viaduct.proxy.rlwy.net",
-    port: 22191,
-    user: "root",
-    password: "xkqACHITfXhIULKQHTHbHnuCXTaZVZgF",
-    database: "railway"
+    MYSQLHOST: "viaduct.proxy.rlwy.net",
+    portMYSQLPORT: 22191,
+    MYSQLUSER: "root",
+    MYSQLPASSWORD: "xkqACHITfXhIULKQHTHbHnuCXTaZVZgF",
+    MYSQLDATABASE: "railway"
 }
 
-export default db
+export default db 
+/* const db = {
+    RAILWAY_RDS_HOSTNAME:" viaduct.proxy.rlwy.net",
+    RAILWAY_RDS_PORT: 22191,
+    RAILWAY_RDS_USERNAME:"root",
+    RAILWAY_RDS_PASSWORD: "xkqACHITfXhIULKQHTHbHnuCXTaZVZgF",
+    RAILWAY_RDS_DB_NAME: "railway"
+}; 
+export default db */
